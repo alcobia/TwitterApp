ebc6c3e16384983dcfe6f16b1ef92bb2d562400f
Tiago Sousas <allcobia@live.com.pt>
2011-05-12 10:57:19 +0100

    FirstCommit

---
+++ TwiterApp2/TwiterApp2Tests/TwiterApp2Tests.m
@@ -0,0 +1,33 @@
+//
+//  TwiterApp2Tests.m
+//  TwiterApp2Tests
+//
+//  Created by Luís Alcobia on 12/05/2011.
+//  Copyright 2011 ArquiConsult. All rights reserved.
+//
+
+#import "TwiterApp2Tests.h"
+
+
+@implementation TwiterApp2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TwiterApp2Tests");
+}
+
+@end
